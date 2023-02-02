e667e0782609d4e2b96a4f778c9cd78ea125cf7f
Ganjar Hadiatna <ganjardbc@gmail.com>
2023-02-02 14:10:22 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Greetings />
+  </div>
+</template>
+
+<script>
+import Greetings from './components/Greetings'
+
+export default {
+  name: 'App',
+  components: {
+    Greetings
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: calc(100vh - 450px);
+}
+</style>
---
+++ src/components/Greetings.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="App">
+    <img alt="Sajiin logo" src="../assets/logo-sajiin.png" style="height: 120px;">
+    <h1>Serve You Soon.</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
